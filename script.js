7d8935231ff02fbc7c3ba06f1ac39deb0de4b1a2
Zi <104781216+ZairaBarry@users.noreply.github.com>
2022-11-09 18:33:57 -0400

    assigned the requiremenst for password length

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+'use strict';
+
+//Assign the length of the password
+function generatePassword() {
+  var passLength = prompt('Choose the length of your password. It must be at least 8  characters and no more than 128 characters long.')
+  console.log(passLength);
+
+if (passLength < 8 || passLength > 128 ) {
+     alert ( 'Attn: It must be at least 8  characters and no more than 128 characters long.')
+     return ;
+  }}
+  
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -8,7 +20,10 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+   
+ 
+ }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
