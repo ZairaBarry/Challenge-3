6c0245d7e797a011a922b6af48bd539122e889f5
Zi <104781216+ZairaBarry@users.noreply.github.com>
2022-11-10 18:14:43 -0400

    generating password that matches all selected criteria

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function generatePassword() {
   if (passLength < 8 || passLength > 128 ) {
     alert ( 'Attn: It must be at least 8  characters and nomore than 128 characters long.')
     return ;
-  }
+  };
   
   //Asigning the type of characters
   var lowerCase = confirm (' Do you want to include lower case characters in your password?');
@@ -16,17 +16,43 @@ function generatePassword() {
   var numeric = confirm (' Do you want to include numeric characters in your password?');
   var specialChar = confirm (' Do you want to include special characters in your password?');
 
-}
+
 // Including If statement  that at least one character should be selected
 if (!(lowerCase || upperCase ||  numeric || specialChar )) {
   alert ( 'You must choose at least one character');
   return;
 };
 
+//Generating password that mathes the selected criteria
 
+var charList = '';
+  if (lowerCase) {
+    charList = charList + 'abcdefghijklmnopqrstuvwxyz' 
+  } if(upperCase) {
+    charList = charList + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  } if (numeric) {
+    charList = charList + '0123456789'
+  } if (specialChar) {
+    charList = charList + '"!#$%&()*+,-.:;<=>?@[\^]_`{|}~'
+  }
 
+  var password = '';
+
+  var createPassword = function () {
+    
+  
+    for (var i = 0 , n = charList.length; i < passLength; ++i){
+      password = password + charList.charAt(Math.floor(Math.random()*n));
+    }
 
   
+  };
+
+  createPassword();
+
+  return password;
+     
+}
 // Assignment Code
   var generateBtn = document.querySelector("#generate");
 
